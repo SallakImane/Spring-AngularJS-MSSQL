b0e6d5debdef3d411865c37a313dfa62e509a3f7
RAFIKI Houssam-Eddine <houssam-eddine.rafiki@findl.lu>
2018-02-14 11:43:29 +0000

    Step 1 - Empty Spring Data Project

---
+++ DAO-Example/src/main/java/lu/findl/dao/DaoExampleApplication.java
@@ -0,0 +1,12 @@
+package lu.findl.dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoExampleApplication.class, args);
+	}
+}
