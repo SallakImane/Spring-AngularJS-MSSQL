4d40dd68dfe2599de11204d85d7e9a8e8fa1a76f
RAFIKI Houssam-Eddine <houssam-eddine.rafiki@findl.lu>
2018-02-14 13:04:08 +0000

    Step 2 - Empty Spring Web Project

---
+++ WebApi-Example/src/main/java/lu/findl/webapi/WebApiExampleApplication.java
@@ -0,0 +1,12 @@
+package lu.findl.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApiExampleApplication.class, args);
+	}
+}
